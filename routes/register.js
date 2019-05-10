65c9c05930fea129d760c737209aa607f71c443b
Elysmeere <48969307+Elysmeere@users.noreply.github.com>
2019-05-10 18:59:26 -0400

    Update register.js

--- routes/register.js
+++ routes/register.js
@@ -1,27 +1,35 @@
 const express = require("express");
 const router = express.Router();
-const xss = require("xss");
 const data = require("../data");
+const bcrypt = require("bcryptjs");
 router.get("/",async (req,res) =>{
     // console.log("/ dir");
     res.render("admin/register", {});
 });
-router.get("/:user", async (req,res) =>{
-    console.log(req.params.user);
-    
-    await data.userData.addUser(req.params.user,"password","guangqi","qing","gqing@stevens.edu");
-    res.json({hello:"world"});
-});
+
 router.post("/", async (req,res) =>{
-    
+    console.log(req.body);
+    let userInfo = req.body;
+    let hashedPassword = bcrypt.hashSync(userInfo.password, 3);
+    try {
+        await data.users.create(userInfo.firstName,userInfo.lastName, userInfo.email, hashedPassword);
+        res.redirect("/login");
+    } catch (err) {
+        // console.log(err);
+        res.render("admin/error",{error: err});
+    }
 });
-router.post("/:user", async (req,res) =>{
-    console.log(req.params.user);
-    const userData = data.userData;
-    let user = await userData.findUser(req.params.user);
-    if(user)
-        res.send({message:"true"});
-    else
-        res.send({message:"false"});
+router.get("/:email", async (req,res) =>{
+    // console.log(req.params.user);
+    try {
+        let user = await data.users.getByEmail(req.params.user);
+        // console.log("a");
+        // console.log(user);
+        // console.log("a");
+        res.sendStatus(200);
+    } catch (err) {
+        // console.log("a");
+        res.sendStatus(404);
+    }
 });
 module.exports = router;
