ea354a2e34cc4755a41e29cd2b5067a70f91e161
Elysmeere <48969307+Elysmeere@users.noreply.github.com>
2019-05-10 20:12:22 -0400

    Update register.js

--- public/js/register.js
+++ public/js/register.js
@@ -1,14 +1,16 @@
 $(function(){
-    $("#username").blur(function(){
+    $("#email").blur(function(){
         let username = this.value;
-        alert("alert");
+        
         $.ajax({
             url: "/register/"+username,
             type: 'GET',
-            dataType: 'json', // added data type
+            // dataType: 'json', // added data type
             success: function(res) {
-                console.log("abc");
-                alert(res);
+                $("#checkUsername").attr({type:"textarea"}).val("Sorry, email exists");
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+                $("#checkUsername").attr({type:"textarea"}).val("You can use this email");
             }
         });
     });
